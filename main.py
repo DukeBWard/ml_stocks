d2cc058187b8d5d06d648cd58670a948e7a54f0d
Luke Ward <wardlukeb@gmail.com>
2024-06-10 13:11:23 -0400

    added scatter plot

--- main.py
+++ main.py
@@ -16,13 +16,22 @@ selected_stock = sl.selectbox("Select the stock you want for prediciton", stocks
 num_years = sl.slider("Years of prediction", 1, 10)
 period = num_years * 365
 
-
+# helpful cache decorator for streamlit
+@sl.cache_data
 def get_data(ticker):
+    # this data is a pandas dataframe
     data = yahoo.download(ticker, START, TODAY)
     # puts date in first column
     data.reset_index(inplace=True)
     return data
 
+def plot_data():
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=data['Date'], y=data['Open']))
+
 data_state = sl.text("Loading data...")
 data = get_data(selected_stock)
 data_state.text("Loaded data!")
+
+sl.subheader("Raw stock data")
+sl.write(data.tail())
